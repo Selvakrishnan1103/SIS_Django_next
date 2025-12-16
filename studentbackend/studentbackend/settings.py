89b2cd050c342406644861cc7658d748748c9773
ajaykanna2007 <ajay@zionstar.in>
2025-12-16 23:47:09 +0530

    sql added

--- studentbackend/studentbackend/settings.py
+++ studentbackend/studentbackend/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -95,10 +99,16 @@ WSGI_APPLICATION = 'studentbackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'django_user',
+        'PASSWORD': 'Django@123!',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
 # Password validation
